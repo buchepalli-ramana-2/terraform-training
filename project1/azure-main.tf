e31c113b262e61287e8273eb398c6a95391673fd
buchepalli-ramana-2 <buchepalli.ramana@hotmail.com>
2023-12-22 12:19:40 +0530

    testing cost estiamtion option in tf cloud

--- project1/azure-main.tf
+++ project1/azure-main.tf
@@ -8,6 +8,13 @@ resource "azurerm_resource_group" "name" {
 
 }
 
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "cost-testing-public-ip"
+  location            = azurerm_resource_group.name.location
+  resource_group_name = azurerm_resource_group.name.name
+  allocation_method   = "Dynamic"
+}
+
 
 # resource "aws_s3_bucket" "example" {
 #   bucket = "my-tf-train-bucket"
